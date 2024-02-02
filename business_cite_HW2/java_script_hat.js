7ab4eeb36f32d5f22fb8074ae9bb4a8ac19883e6
C3PO3 <cordelia.ludden@gmail.com>
2024-02-02 16:53:50 -0500

    upload background change

---
+++ business_cite_HW2/java_script_hat.js
@@ -0,0 +1,11 @@
+
+
+function Random() {
+    var rnd = Math.floor(Math.random() * 10);
+    document.getElementById('tb').value = rnd;
+}
+
+function Random2() {
+    var rnd = Math.floor(Math.random() * 10);
+    document.getElementById('value1').value = rnd;
+}
